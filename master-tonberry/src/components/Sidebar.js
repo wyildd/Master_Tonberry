a506e0833cf899e70cc36be1ca3c0304d5497d8f
Donnyv116 <donnyv116@gmail.com>
2018-04-16 18:54:55 -0700

    burger menu restored

--- master-tonberry/src/components/Sidebar.js
+++ master-tonberry/src/components/Sidebar.js
@@ -8,21 +8,22 @@ const Sidebar = () => (
   //   event.preventDefault();
     
   // }
-
+    <Menu>
       <ul className="menu-item">
-        <li className={window.location.pathname === "/" ? "active" : ""}>
+        <li id="home" className={window.location.pathname === "/" ? "active" : ""}>
         <Link to="/">Home</Link>
         </li>
-        <li className={window.location.pathname === "/learn" ? "active" : ""}>
+        <li id="learn" className={window.location.pathname === "/learn" ? "active" : ""}>
         <Link to="/learn">Learn</Link>
         </li>
-        <li className={window.location.pathname === "/create" ? "active" : ""}>
+        <li id="create" className={window.location.pathname === "/create" ? "active" : ""}>
         <Link to="/create">Create</Link>
         </li>
-        <li className={window.location.pathname === "/server" ? "active" : ""}>
+        <li id="serve" className={window.location.pathname === "/server" ? "active" : ""}>
         <Link to="/server">Server</Link>
         </li>
       </ul>
+      </Menu>
     );
   
 // Do we need setting?
