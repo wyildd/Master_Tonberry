90c262159639c97f763b5de63ebe04d8b6e8c924
Landon <landonchristopherrichards@gmail.com>
2018-04-16 21:26:37 -0700

    believe in urself

---
+++ master-tonberry/src/components/Create.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class Create extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      isbn: '',
+      title: '',
+      author: '',
+      description: '',
+      published_year: '',
+      publisher: ''
+    };
+  }
+  onChange = (e) => {
+    const state = this.state
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const { isbn, title, author, description, published_year, publisher } = this.state;
+
+    axios.post('/api/book', { isbn, title, author, description, published_year, publisher })
+      .then((result) => {
+        this.props.history.push("/")
+      });
+  }
+
+  render() {
+    const { isbn, title, author, description, published_year, publisher } = this.state;
+    return (
+      <div class="container">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">
+              Add a new guide for other users!
+            </h3>
+          </div>
+          <div class="panel-body">
+            <h4><Link to="/"><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> Tonberry List</Link></h4>
+            <form onSubmit={this.onSubmit}>
+              <div class="form-group">
+                <label for="isbn">Game Name:</label>
+                <input type="text" class="form-control" name="isbn" value={isbn} onChange={this.onChange} placeholder="Game Name" />
+              </div>
+              <div class="form-group">
+                <label for="title">Title of the Boss:</label>
+                <input type="text" class="form-control" name="title" value={title} onChange={this.onChange} placeholder="Title" />
+              </div>
+              <div class="form-group">
+                <label for="author">Author:</label>
+                <input type="text" class="form-control" name="author" value={author} onChange={this.onChange} placeholder="Author" />
+              </div>
+              <div class="form-group">
+                <label for="description">Description:</label>
+                <textArea class="form-control" name="description" onChange={this.onChange} placeholder="Description" cols="80" rows="3">{description}</textArea>
+              </div>
+              <div class="form-group">
+                <label for="published_date">Balance Patch:</label>
+                <input type="number" class="form-control" name="published_year" value={published_year} onChange={this.onChange} placeholder="Use only if applicable" />
+              </div>
+              <div class="form-group">
+                <label for="publisher">Enter Your Guide here!</label>
+                <input type="text" class="form-control" name="publisher" value={publisher} onChange={this.onChange} placeholder="Placeholder" />
+              </div>
+              <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Create;
--- master-tonberry/src/index.js
+++ master-tonberry/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Login from './components/Login';
+import Register from './components/Register';
+import Edit from './components/Edit';
+import Create from './components/Create';
+import Show from './components/Show';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+      <div>
+        <Route exact path='/' component={App} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/edit/:id' component={Edit} />
+        <Route path='/create' component={Create} />
+        <Route path='/show/:id' component={Show} />
+      </div>
+  </Router>,
+  document.getElementById('root')
+);
 registerServiceWorker();
